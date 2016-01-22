1d3b73491306bc0f2e415808fdeda5fea782f99f
Mike Wilkerson <wilkerson@mac.com>
2016-01-22 07:02:50 -0600

    add data validations

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -4,4 +4,15 @@ class Restaurant < ActiveRecord::Base
   
   has_many :reviews
 
+  validates :name, :address, :phone, :website, :image, presence: true
+
+  validates :phone, format: { with: /\A\(\d{3}\) \d{3}-\d{4}\z/,
+    message: "must use the format (123) 456-7890" }
+
+  validates :website, format: { with: /\Ahttps?:\/\/.*\z/,
+    message: "must start with http:// or https://" }
+
+  validates :address, format: { with: /\A\d+[^,]+,[^,]+, [A-Z]{2} \d{5}\z/,
+    message: "must use the format 123 Fifth Avenue, New York, NY 10118" }
+
 end
